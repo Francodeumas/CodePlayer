b61ff6e7c8c5f35281395700f4a5f424ef9030da
unknown <francoselva@Francoselva-zev.impelsys.com>
2017-02-14 17:39:19 +0530

    updated files

--- js/script.js
+++ js/script.js
@@ -17,33 +17,71 @@
 			}
 		},
 
-		ajaxRequest : function(Elemvalue){
+		ajaxGetRequest : function(Elemvalue){
 			var xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function(){
 				if(this.readyState === 4 && this.status === 200){
-					document.getElementById('codeEditor').innerHTML = this.responseText;
+					document.getElementById('codeEditor').value = this.responseText;
 				}
 			}
-			xhttp.open('GET','files/'+Elemvalue.toLowerCase(),false);
+			xhttp.open('GET','files/'+Elemvalue.toLowerCase(),true);
 			xhttp.send();
 		},
 
+		ajaxPostRequest : function(element,editorValue){
+		   	// var xhttp = new XMLHttpRequest();
+		   	// var url = encodeURIComponent("files/"+element);
+		    // var value = "value="+editorValue;
+		   	// xhttp.open('POST',url,true);
+		   	// xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		   	// xhttp.send(value);
+		},
+
 		getFiles : function(Elemvalue){
-           this.ajaxRequest(Elemvalue);
+           this.ajaxGetRequest(Elemvalue);
+		},
+
+		execute : function(){
+			var codeEditorValue = document.getElementById('codeEditor').value,
+			    result = document.getElementById('result');
+			    result.innerHTML = codeEditorValue;
+		},
+
+		pushDataToFiles : function(){
+				    var button = document.getElementsByTagName('button'),
+				        codeEditorValue = document.getElementById('codeEditor').value;
+				    for(var i=0; i<button.length; i++){
+				    	if(button[i].hasAttribute('class')){
+				    		this.ajaxPostRequest(button[i].innerHTML.toLowerCase(),codeEditorValue);
+				    	}
+				    }
 		}
 	}
 
 	 var events = {
 	 		filesViewEvent : function(){
 	 			var filesView = document.getElementById('filesView'),
-	 			     button = document.getElementsByTagName('button');
+	 			     button = document.getElementsByTagName('button'),
+	 			     codeEditor = document.getElementById('codeEditor');
 	 				filesView.addEventListener('click',function(event){
 	 					logics.toggleButtonStyle(event.target);
 	 					logics.getFiles(event.target.innerHTML);
 	 				})
+	 				codeEditor.addEventListener('keyup',function(){
+	 					logics.pushDataToFiles();
+	 					var buttons = document.getElementsByTagName('button');
+	 					   for(var i=0; i<buttons.length; i++){
+	 					   	 if(button[i].id === "index" && button[i].hasAttribute('class')){
+	 							logics.execute();   	 	
+	 					   	 }
+	 					   }
+	 					
+	 				})
+
 	 		}
 	 }
 
 	 events.filesViewEvent();
+	 logics.ajaxGetRequest('Index.html');
 
 })();
